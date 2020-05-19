e48eaef4aeacfecf51fb11a8df0259da090bf5d9
Hani Audah <ht.audah@gmail.com>
2020-05-19 17:31:22 +0300

    Added playbook for creating AD environment

---
+++ README.md
@@ -0,0 +1,6 @@
+# VMC Lab Environment
+A collection of scripts and Ansible playbooks required to provision a complete VMC lab environment
+
+## Prepare OS templates
+Many playbook tasks assume certain templates to exist in the lab's vSphere environment. Prepare the
+following
---
+++ prepare_ansible.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# A shell script to prepare the Ansible control node for the VMC lab
+# Written by: Hani Audah <ht.aramco@gmail.com>
+# Last updated on: May/17/2020
+# -------------------------------------------------------------------------------------------------
+sudo dnf install ansible
