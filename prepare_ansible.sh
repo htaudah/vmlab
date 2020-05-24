839b46441465a2b5aba32dd550641354efd2ebcb
Hani Audah <ht.audah@gmail.com>
2020-05-24 16:55:42 +0300

    WIP: Fixing customization

--- prepare_ansible.sh
+++ prepare_ansible.sh
@@ -3,5 +3,6 @@
 # Written by: Hani Audah <ht.aramco@gmail.com>
 # Last updated on: May/17/2020
 # -------------------------------------------------------------------------------------------------
-sudo dnf install ansible
-sudo pip install --upgrade git+https://github.com/vmware/vsphere-automation-sdk-python.git
+sudo dnf install ansible -y
+pip install --upgrade git+https://github.com/vmware/vsphere-automation-sdk-python.git
+sed -i -r 's/^#(log_path.*)/\1/' /etc/ansible/ansible.cfg
