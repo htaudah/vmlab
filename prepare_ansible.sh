f468baa05f25bf40102c2e0758d21bea4140b133
Hani Audah <ht.audah@gmail.com>
2020-06-24 12:52:06 +0300

    Added Windows file share servers

--- prepare_ansible.sh
+++ prepare_ansible.sh
@@ -3,9 +3,13 @@
 # Written by: Hani Audah <ht.aramco@gmail.com>
 # Last updated on: May/17/2020
 # -------------------------------------------------------------------------------------------------
-sudo dnf install ansible -y
+sudo dnf -y install ansible
+sudo dnf -y install krb5-devel krb5-workstation
 pip install --upgrade git+https://github.com/vmware/vsphere-automation-sdk-python.git
 # Needed for ldap_entry module (TODO: consider alternatives?)
 dnf install python3-devel openldap-devel gcc -y
 pip install python-ldap
+# Needed for CredSSP authentication to Windows hosts
+pip install ntlm-auth --upgrade
+pip install pywinrm[credssp]
 sed -i -r 's/^#(log_path.*)/\1/' /etc/ansible/ansible.cfg
