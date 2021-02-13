2eb7e31b7c807cdd2b445cfdddb4c2295c0fd703
Hani Audah <ht.audah@gmail.com>
2021-02-14 01:47:28 +0300

    Using root where needed for prepare_ansible and added aws keys to sample vault

--- prepare_ansible.sh
+++ prepare_ansible.sh
@@ -6,11 +6,11 @@
 sudo dnf -y install epel-release
 sudo dnf -y install ansible
 # Needed for ldap_entry module (TODO: consider alternatives?)
-dnf install python3-devel openldap-devel gcc -y
+sudo dnf install gcc python3-devel openldap-devel -y
 pip3 install python-ldap
 # Needed for CredSSP authentication to Windows hosts
 pip3 install ntlm-auth --upgrade
 pip3 install pywinrm[credssp]
-sed -i -r 's/^#(log_path.*)/\1/' /etc/ansible/ansible.cfg
+sudo sed -i -r 's/^#(log_path.*)/\1/' /etc/ansible/ansible.cfg
 # These collections provide additional functionality not yet available in base
 # TODO: switching to AWS; no longer need vmware collections
