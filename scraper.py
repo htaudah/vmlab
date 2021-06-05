ff645638a67d7a9db288b285080e72df270b5e3e
Hani Audah <ht.audah@gmail.com>
2021-06-05 21:27:05 +0300

    Adding WS1 to scraper

--- scraper.py
+++ scraper.py
@@ -6,7 +6,8 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 browser = webdriver.Chrome('/usr/local/share/chromedriver', options=chrome_options)
 browser.implicitly_wait(10)
-url = ("https://uemc.haramco.org/AirWatch")
+uem_url = ("https://uemc.haramco.org/AirWatch")
+access_url = ("https://uemc.haramco.org/AirWatch")
 browser.get(url)
 browser.find_element_by_id('UserName').send_keys('Administrator')
 browser.find_element_by_id('UserName').submit()
@@ -28,6 +29,21 @@ browser.find_elements_by_css_selector('a[href="/AirWatch/Settings/CategoryLandin
 browser.find_elements_by_css_selector('a[href="/AirWatch/ContentGateway"]')[1].click()
 browser.find_element_by_css_selector('label[for="ContentGatewayEnabled_True"]').click()
 browser.find_element_by_css_selector('input[name="Save"]').click()
+
+# WS1 Access
+browser.find_element_by_id('pass').send_keys('Baltona31@')
+browser.find_element_by_id('passConf').send_keys('Baltona31@')
+browser.find_element_by_id('rootPass').send_keys('Baltona31@Baltona31@')
+browser.find_element_by_id('rootPassConf').send_keys('Baltona31@Baltona31@')
+browser.find_element_by_id('sshPass').send_keys('Baltona31@Baltona31@')
+browser.find_element_by_id('sshPassConf').send_keys('Baltona31@Baltona31@')
+browser.find_element_by_id('nextButton').click()
+
+browser.find_element_by_css_selector('input.externDB').click()
+browser.find_element_by_id('jdbcurl').send_keys('jdbc:sqlserver://MainAG:1433;DatabaseName=accessdb')
+browser.find_element_by_id('dbUsername').send_keys('temp_acct')
+browser.find_element_by_id('dbPassword').send_keys('Baltona31@')
+browser.find_element_by_id('nextButton').click()
 
 html_source = browser.page_source
 browser.quit()
