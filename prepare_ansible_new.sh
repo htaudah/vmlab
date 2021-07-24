040c8daf6016fdcf880f59f294259bb84cf0de48
Hani Audah <ht.audah@gmail.com>
2021-07-24 23:20:48 +0300

    Adding django api server

--- prepare_ansible_new.sh
+++ prepare_ansible_new.sh
@@ -8,9 +8,6 @@ pip3 install ansible
 git clone https://github.com/vmware/vsphere-automation-sdk-python.git /tmp/vsphere-sdk
 pip3 install --upgrade -r /tmp/vsphere-sdk/requirements.txt --extra-index-url file:///tmp/vsphere-sdk/lib
 pip3 install --upgrade git+https://github.com/vmware/vsphere-automation-sdk-python.git
-# Needed for ldap_entry module (TODO: consider alternatives?)
-#dnf install python3-devel openldap-devel gcc -y
-#pip3 install python-ldap
 # Needed for CredSSP authentication to Windows hosts
 pip3 install ntlm-auth --upgrade
 pip3 install pywinrm[credssp]
