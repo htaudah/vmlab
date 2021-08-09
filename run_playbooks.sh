8c038b6d45156fe5c64f37d2d23f049f6d8d622a
Hani Audah <ht.audah@gmail.com>
2021-08-10 00:16:10 +0300

    Switching to james

--- run_playbooks.sh
+++ run_playbooks.sh
@@ -3,5 +3,5 @@
 # Written by: Hani Audah <ht.aramco@gmail.com>
 # Last updated on: Aug/03/2021
 # -------------------------------------------------------------------------------------------------
-echo "${@: 2}" > /tmp/output
-#ansible-playbook -i inventory.ini --vault-password-file /tmp/vault-pass --extra-vars "${@: 2}" "$1"
+#echo "${@: 2}" > /tmp/output
+ansible-playbook -i inventory.ini --vault-password-file /tmp/vault-pass --extra-vars "${@: 2}" "$1"
